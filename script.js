9d4b343a842fc0e7a86f20b0a85f05ce58ff33f6
Matthew-Bueno <Matthew.bueno09@gmail.com>
2026-04-20 16:09:36 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,178 @@
+let lives = 3; // This is how many lives you will have
+let playerRow = 2; // What row the player will spawns
+let playerCol = 2; //  What column the player will spawn
+let enemyRedRow = 4; // What row the red enemy will spawn
+let enemyRedCol = 4; // What column the red enemy will spawn
+let enemyGreyRow = 4; // What row the green enemy will spawn
+let enemyGreyCol = 0; // What column the green enemy will spawn
+let enemyOrangeRow = 0; // What row the orange enemy will spawn
+let enemyOrangeCol = 4; // What column the orange enemy will spawn
+let enemyPinkRow = 0; // What row the white enemy will spawn
+let enemyPinkCol = 0; // What column the white enemy will spawn
+let redInterval;
+let container = document.getElementById("container"); // This is getting the container id that will be used later to hide the rules
+
+function play() {
+  // This function is used when you click play everthing will start
+  container.style.display = "none";
+
+  document.getElementById("grid").style.display = "grid"; //
+
+  lives = 3;
+  updateHearts();
+  updatePlayerPosition(); // This is for the player can load on the grid
+  enemyPink();
+  enemyOrange();
+  enemyGrey();
+  enemyRed();
+  redInterval = setInterval(enemyRedMovement, 700);
+  document.addEventListener("keydown", playerMove); // This event is used when you press down a key
+};
+
+function updateHearts() {
+  // This function is to upadte the hearts as well as load them onto the HMTL screen
+  const heartsDiv = document.getElementById("hearts");
+
+  let heartDisplay = "";
+
+  for (let i = 0; i < lives; i++) {
+    heartDisplay += "♥️";
+  };
+
+  heartsDiv.innerHTML = `<p>${heartDisplay}</p>`;
+}
+
+function playerMove(event) {
+  // This function is used when
+  let key = event.key.toLowerCase();
+
+  if (key === "w" || key === "arrowup") {
+    if (playerRow > 0) playerRow--;
+  } else if (key === "s" || key === "arrowdown") {
+    if (playerRow < 4) playerRow++;
+  } else if (key === "a" || key === "arrowleft") {
+    if (playerCol > 0) playerCol--;
+  } else if (key === "d" || key === "arrowright") {
+    if (playerCol < 4) playerCol++;
+  };
+
+  updatePlayerPosition();
+  enemyHit();
+};
+
+function updatePlayerPosition() {
+  const player = document.getElementById("player");
+
+  const cellWidth = 130;
+  const cellHeight = 110;
+
+  const playerSize = 20;
+
+  const offsetX = (cellWidth - playerSize) / 2;
+  const offsetY = (cellHeight - playerSize) / 2;
+
+  player.style.transform = `translate(${playerCol * cellWidth + offsetX}px, ${playerRow * cellHeight + offsetY}px)`;
+};
+
+function enemyPink() {
+  const enemyPink = document.getElementById("enemyPink");
+
+  const enemyPinkcellWidth = 130;
+  const enemyPinkcellHeight = 110;
+
+  const enemyPinkSize = 20;
+
+  const offsetX = (enemyPinkcellWidth - enemyPinkSize) / 2;
+  const offsetY = (enemyPinkcellHeight - enemyPinkSize) / 2;
+
+  enemyPink.style.transform = `translate(${enemyPinkCol * enemyPinkcellWidth + offsetX}px, ${enemyPinkRow * enemyPinkcellHeight + offsetY}px)`;
+};
+
+function enemyPinkMovment(){
+
+};
+
+function enemyOrange() {
+  const enemyOrange = document.getElementById("enemyOrange");
+
+  const enemyOrangecellWidth = 130;
+  const enemyOrangecellHeight = 110;
+
+  const enemyOrangeSize = 20;
+
+  const offsetX = (enemyOrangecellWidth - enemyOrangeSize) / 2;
+  const offsetY = (enemyOrangecellHeight - enemyOrangeSize) / 2;
+
+  enemyOrange.style.transform = `translate(${enemyOrangeCol * enemyOrangecellWidth + offsetX}px, ${enemyOrangeRow * enemyOrangecellHeight + offsetY}px)`;
+};
+
+function enemyOrangeMovment(){
+
+};
+
+function enemyGrey() {
+  const enemyGrey = document.getElementById("enemyGrey");
+
+  const enemyGreycellWidth = 130;
+  const enemyGreycellHeight = 110;
+
+  const enemyGreySize = 20;
+
+  const offsetX = (enemyGreycellWidth - enemyGreySize) / 2;
+  const offsetY = (enemyGreycellHeight - enemyGreySize) / 2;
+
+  enemyGrey.style.transform = `translate(${enemyGreyCol * enemyGreycellWidth + offsetX}px, ${enemyGreyRow * enemyGreycellHeight + offsetY}px)`;
+};
+
+function enemyGreyMovement() {
+
+};
+
+function enemyRed() {
+  const enemyRed = document.getElementById("enemyRed");
+
+  const enemyRedcellWidth = 130;
+  const enemyRedcellHeight = 110;
+
+  const enemyRedSize = 20;
+
+  const offsetX = (enemyRedcellWidth - enemyRedSize) / 2;
+  const offsetY = (enemyRedcellHeight - enemyRedSize) / 2;
+
+  enemyRed.style.transform = `translate(${enemyRedCol * enemyRedcellWidth + offsetX}px, ${enemyRedRow * enemyRedcellHeight + offsetY}px)`;
+};
+
+function enemyRedMovement() {
+  console.log("enemyRedMovement");
+  if (enemyRedRow < playerRow) {
+    enemyRedRow++;
+  } else if (enemyRedRow > playerRow) {
+    enemyRedRow--;
+  };
+
+  if (enemyRedCol < playerCol) {
+    enemyRedCol++;
+  } else if (enemyRedCol > playerCol) {
+    enemyRedCol--;
+  };
+
+  enemyRed();
+  setTimeout(() => enemyHit(), 1180);
+};
+
+function enemyHit() {
+  if (playerRow === enemyRedRow && playerCol === enemyRedCol) {
+    lives--;
+    updateHearts();
+    enemyRedRow = 4;
+    enemyRedCol = 4;
+
+    setTimeout(() => enemyRed(), 700);
+    clearInterval(redInterval);
+    setTimeout(() => (redInterval = setInterval(enemyRedMovement, 600)), 1180);
+  };
+};
+
+function gameOver(){
+  
+}
